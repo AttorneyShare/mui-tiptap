e13d1701cfabfdae474f53e885d77b0468445562
Jeremy Kahn <jeremy@attorneyshare.com>
2024-04-22 15:02:44 -0500

    chore: demonstrate how to work with plaintext

--- src/demo/Editor.tsx
+++ src/demo/Editor.tsx
@@ -17,7 +17,14 @@ import EditorMenuControls from "./EditorMenuControls";
 import useExtensions from "./useExtensions";
 
 const exampleContent =
-  '<h2 style="text-align: center">Hey there 👋</h2><p>This is a <em>basic</em> example of <code>mui-tiptap</code>, which combines <a target="_blank" rel="noopener noreferrer nofollow" href="https://tiptap.dev/">Tiptap</a> with customizable <a target="_blank" rel="noopener noreferrer nofollow" href="https://mui.com/">MUI (Material-UI)</a> styles, plus a suite of additional components and extensions! Sure, there are <strong>all <em>kinds</em> of <s>text</s> <u>formatting</u> options</strong> you’d probably expect from a rich text editor. But wait until you see the <span data-type="mention" data-id="15" data-label="Axl Rose">@Axl Rose</span> mentions and lists:</p><ul><li><p>That’s a bullet list with one …</p></li><li><p>… or two list items.</p></li></ul><p>Isn’t that great? And all of that is editable. <strong><span style="color: #ff9900">But wait, </span><span style="color: #403101"><mark data-color="#ffd699" style="background-color: #ffd699; color: inherit">there’s more!</mark></span></strong> Let’s try a code block:</p><pre><code class="language-css">body {\n  display: none;\n}</code></pre><p></p><p>That’s only the tip of the iceberg. Feel free to add and resize images:</p><img height="auto" src="https://picsum.photos/600/400" alt="random image" width="350" style="aspect-ratio: 3 / 2"><p></p><p>Organize information in tables:</p><table><tbody><tr><th colspan="1" rowspan="1"><p>Name</p></th><th colspan="1" rowspan="1"><p>Role</p></th><th colspan="1" rowspan="1"><p>Team</p></th></tr><tr><td colspan="1" rowspan="1"><p>Alice</p></td><td colspan="1" rowspan="1"><p>PM</p></td><td colspan="1" rowspan="1"><p>Internal tools</p></td></tr><tr><td colspan="1" rowspan="1"><p>Bob</p></td><td colspan="1" rowspan="1"><p>Software</p></td><td colspan="1" rowspan="1"><p>Infrastructure</p></td></tr></tbody></table><p></p><p>Or write down your groceries:</p><ul data-type="taskList"><li data-checked="true" data-type="taskItem"><label><input type="checkbox" checked="checked"><span></span></label><div><p>Milk</p></div></li><li data-checked="false" data-type="taskItem"><label><input type="checkbox"><span></span></label><div><p>Eggs</p></div></li><li data-checked="false" data-type="taskItem"><label><input type="checkbox"><span></span></label><div><p>Sriracha</p></div></li></ul><blockquote><p>Wow, that’s amazing. Good work! 👏 <br>— Mom</p></blockquote><p>Give it a try and click around!</p>';
+  "Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum \n\nBlah blah blah \n\n The rain in Spain is mostly in the plain.\nWords!\nLine 1\nLine 2 \nLine 3 ";
+
+// NOTE: We need to replace the plaintext newlines with paragraph elements so
+// that TipTap handles the formatting correctly.
+const formattedExampleContent = exampleContent
+  .match(/^.*$/gm)
+  ?.map((line) => `<p>${line}</p>`)
+  .join("");
 
 function fileListToImageFiles(fileList: FileList): File[] {
   // You may want to use a package like attr-accept
@@ -141,7 +148,7 @@ export default function Editor() {
         <RichTextEditor
           ref={rteRef}
           extensions={extensions}
-          content={exampleContent}
+          content={formattedExampleContent}
           editable={isEditable}
           editorProps={{
             handleDrop: handleDrop,
@@ -201,9 +208,28 @@ export default function Editor() {
                   variant="contained"
                   size="small"
                   onClick={() => {
-                    setSubmittedContent(
-                      rteRef.current?.editor?.getHTML() ?? ""
-                    );
+                    const editor = rteRef.current?.editor;
+
+                    if (!editor) {
+                      throw new Error();
+                    }
+
+                    const formattedContent = editor.getHTML();
+
+                    // NOTE: This re-inserts the "\n" characters that were
+                    // present in the original exampleContent string. There is no
+                    // HTML embedded in this string.
+                    const plaintextContent: string =
+                      editor.state.doc.content.content
+                        .map((content) => content.textContent)
+                        .join("\n");
+
+                    console.log({
+                      formattedContent,
+                      plaintextContent,
+                    });
+
+                    setSubmittedContent(editor.getHTML() ?? "");
                   }}
                 >
                   Save
